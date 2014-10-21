57a60c7209722f599182f8599e534ceed72cd4fb
pozdnart <pozdnart@fit.cvut.cz>
2014-10-21 17:43:12 +0200

    Snad to de

--- floydwarschall.hpp
+++ floydwarschall.hpp
@@ -1,9 +1,21 @@
 #include "mygraph.hpp"
-#include <memory>
+#include <limits>
 
 using namespace std;
 
 void floydMarschall(MyGraph * graph) {
-    //unsigned int size = graph->size();
-    //unsigned int cesta[size][size];
+    double ** matrix = graph->getMatrix();
+    unsigned int size = graph->size();
+
+    for(unsigned int k = 0; k < size; ++k)
+        for(unsigned int i = 0; i < size; ++i)
+            for(unsigned int j = 0; j < size; ++j) {
+                matrix[i][j] = min(matrix[i][j], matrix[i][k] + matrix[k][j]);
+            }
+
+    for(unsigned int k = 0; k < size; ++k)
+        for(unsigned int i = k+1; i < size; ++i)
+            if(matrix[k][i] != numeric_limits<double>::max())
+                cout << k << " " << i << " " << matrix[k][i] << endl;
+
 }
